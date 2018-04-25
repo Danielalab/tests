a6b62ea03e10c6dff7fbcd340cb92d39c2dbca2a
Danielalab <olabolacrayola@hotmail.com>
2018-04-25 10:27:12 -0500

    test with Mocha and assert Node

--- isVowel.spec.js
+++ isVowel.spec.js
@@ -1,17 +1,22 @@
 // Archivo `isVowel.spec.js`:
-
+ 
+const assert = require('assert');
 const isVowel = require('./isVowel');
 
-console.log('debería devolver true para letra a');
-if (isVowel('a') === true) {
-  console.log('✓ ok');
-} else {
-  console.error('✗ fail');
-}
+const assertEqual = (value, expected) => {
+  if (value !== expected) {
+    throw new Error(`Expected '${value}' to equal '${expected}'`);
+  }
+};
 
-console.log('debería devolver false para letra b');
-if (isVowel('b') === false) {
-  console.log('✓ ok');
-} else {
-  console.error('✗ fail');
-}
+describe('isVowel()', () => {
+
+  it('debería devolver true para letra a', () => {
+    assert.equal(isVowel('a'), true);
+  });
+
+  it('debería devolver false para letra a', () => {
+    assert.equal(isVowel('b'), false);
+  });
+
+});
